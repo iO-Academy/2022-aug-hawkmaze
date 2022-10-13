34e9059762d93dda17bd33ecc6ebf27a9ce479f2
Henry <henryppercy@gmail.com>
2022-10-13 10:05:04 +0100

    Refactored animation code

--- js/animation.js
+++ js/animation.js
@@ -3,24 +3,25 @@
  Constructor for the creatures
  Arrow functions cannot be used as constructors and will throw an error when used with new
 */
-export const Creature = function(element, aniFrame, startPos, endPos, action = 'moveRight') {
+export const dynamicObject = function(element, aniFrame, startPos, endPos, speed, action = 'moveRight') {
     this.element = element;    // For which element you're selecting
     this.aniFrame = aniFrame;  // Empty variable which to be redefined with a callback to the animation function
     this.startPos = startPos;  // The starting position in px
     this.endPos = endPos;      // The ending position in px
     this.action = action;      // For the direction of movement
+    this.speed = speed;        // Set the speed of the object
     this.currentPos = this.startPos;  // Sets the start position of each creature to the current position
 }
 
 // Function to animate each creature using the properties of each object
-export const creatureMoveHoriz = (timestamp, creature) => {
+export const objectMove = (timestamp, creature) => {
     if (creature.action === 'moveRight') {
-        creature.currentPos += 2;
+        creature.currentPos += creature.speed;
         if (creature.currentPos >= creature.endPos) {
             creature.action = 'moveLeft';
         }
     } else {
-        creature.currentPos -= 2;
+        creature.currentPos -= creature.speed;
         if (creature.currentPos <= creature.startPos) {
             creature.action = 'moveRight';
         }
@@ -28,6 +29,6 @@ export const creatureMoveHoriz = (timestamp, creature) => {
     creature.element.style.left = creature.currentPos + 'px';
     creature.aniFrame = requestAnimationFrame(
         (timestamp) => {
-            creatureMoveHoriz(timestamp, creature)
+            objectMove(timestamp, creature)
         });
 }
